0cb2e10c94c198a7fab6aa4621c0d14cc1022b3e
Rían Errity <rian@paradaux.io>
2021-03-15 20:32:28 +0000

    Move getJSON to the common js

--- public/assets/js/friendlybot.js
+++ public/assets/js/friendlybot.js
@@ -1,18 +1,3 @@
-var getJSON = function(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.responseType = 'json';
-    xhr.onload = function() {
-        var status = xhr.status;
-        if (status === 200) {
-            callback(null, xhr.response);
-        } else {
-            callback(status, xhr.response);
-        }
-    };
-    xhr.send();
-};
-
 getJSON('https://api.paradaux.io/friendly-bot/stats', function(err, data) {
     document.getElementById("user-count").innerHTML = data.userCount;
     document.getElementById("guild-count").innerHTML = data.guildCount;
