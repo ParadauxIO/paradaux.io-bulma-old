b2243c5d47643804d0189fe3afd4b4e6744f012d
Rían Errity <rian@paradaux.io>
2021-03-15 20:32:37 +0000

    Tag viewer partially working

---
+++ public/assets/js/tagviewer.js
@@ -0,0 +1,42 @@
+function drawTables() {
+    const urlParams = new URLSearchParams(window.location.search);
+
+    let guildId = urlParams.get("serverid") ?? "No Server Specified"
+
+    if (guildId === undefined || guildId === "No Server Specified") {
+        document.getElementById("tags").innerHTML = "No Server Specified."
+        return;
+    }
+
+    getJSON('https://api.paradaux.io/friendly-bot/tags/' + guildId, function(err, data) {
+        console.log(data)
+
+        data.forEach(element => {
+            console.log(element);
+            document.getElementById("tag-table").innerHTML += `
+<tr style="max-width: available">
+    <th>
+        ${element.id}
+    </th>
+    <th>
+        ${element.content}
+    </th>
+    <th>
+        ${element.timeCreated} UTC
+    </th>
+</tr>`})
+    });
+
+    reloadParticles();
+}
+
+drawTables();
+
+
+
+
+
+
+
+
+
